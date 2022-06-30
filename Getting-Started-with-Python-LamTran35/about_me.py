18844a98338376344d051874903e0e552faf4bfc
Lam Tran <tlambao2@gmail.com>
2022-06-30 11:20:39 -0700

    adding 'getting started with python' activity

---
+++ Getting-Started-with-Python-LamTran35/about_me.py
@@ -0,0 +1,14 @@
+name="Lam Tran"
+preferred="Lam"
+age=18
+used_python=True
+hobbies=["watching movies", "running", "chilling with friends"]
+favorites={"movie": "Knives Out", "food": "tacos", "show": "Superstore"}
+
+print(name, preferred, age, used_python)
+print("\n")
+print(hobbies)
+print("\n")
+print(favorites)
+
+all_vars = dict(vars())
